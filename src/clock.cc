b158f1374bc71cdab1ebb8e439d82fdce0243a71
Adam DeMuri <adam.demuri@gmail.com>
2024-05-06 21:24:13 -0600

    Add define-control for custom clock

--- src/clock.cc
+++ src/clock.cc
@@ -7,6 +7,8 @@ void Error_Handler() {
   while (1);
 }
 
+#ifdef CUSTOM_CLOCK
+
 // Generated using STM32Cube IDE. Sets the clock to use the MSI at 1MHz.
 // According to STM32Cube's power calculator, the processor should consume about
 // 230uA with this configuration.
@@ -17,12 +19,12 @@ void SystemClock_Config() {
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Configure the main internal regulator output voltage
-  */
+   */
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
   RCC_OscInitStruct.MSIState = RCC_MSI_ON;
   RCC_OscInitStruct.MSICalibrationValue = 0;
@@ -33,9 +35,9 @@ void SystemClock_Config() {
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
+                                RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
@@ -45,3 +47,5 @@ void SystemClock_Config() {
     Error_Handler();
   }
 }
+
+#endif  // ifdef CUSTOM_CLOCK
--- src/clock.h
+++ src/clock.h
@@ -1,5 +1,9 @@
 #pragma once
 
+// #define CUSTOM_CLOCK
+
+#ifdef CUSTOM_CLOCK
 extern "C" {
 void SystemClock_Config();
 }
+#endif  // ifdef CUSTOM_CLOCK
