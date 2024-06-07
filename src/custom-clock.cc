818b05f9cff74ad4fe3ee9ba2d88b260c3271e01
Adam DeMuri <adam.demuri@gmail.com>
2024-06-06 19:09:52 -0600

    Move clock to custom-clock to avoid conflict with platform lib

--- src/custom-clock.cc
+++ src/custom-clock.cc
@@ -1,18 +1,6 @@
-// Copyright 2024 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+#include "custom-clock.h"
 
-#include "clock.h"
+#ifdef CUSTOM_CLOCK
 
 #include "stm32l0xx_hal.h"
 
@@ -20,8 +8,6 @@ void Error_Handler() {
   __disable_irq();
   while (1);
 }
-
-#ifdef CUSTOM_CLOCK
 
 // Generated using STM32Cube IDE. Sets the clock to use the MSI at 1MHz.
 // According to STM32Cube's power calculator, the processor should consume about
