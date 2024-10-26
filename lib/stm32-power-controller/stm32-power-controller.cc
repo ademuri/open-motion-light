e0ac1dcecd49550ef35f0d04b5a93d18b37defee
Adam DeMuri <adam.demuri@gmail.com>
2024-10-26 11:39:10 -0600

    Fix sleep mode power consumption

--- lib/stm32-power-controller/stm32-power-controller.cc
+++ lib/stm32-power-controller/stm32-power-controller.cc
@@ -1,6 +1,7 @@
 #include "stm32-power-controller.h"
 
 #include <STM32LowPower.h>
+#include <Wire.h>
 #include <types.h>
 
 #include "pins.h"
@@ -28,20 +29,27 @@ void Stm32PowerController::AttachInterruptWakeup(uint32_t pin, uint32_t mode) {
 }
 
 void Stm32PowerController::Sleep(uint32_t ms) {
+  Wire.end();
+  pinMode(kPinScl, INPUT_ANALOG);
+  pinMode(kPinSda, INPUT_ANALOG);
+
+  pinMode(kPinBatteryLed1, INPUT_ANALOG);
+  pinMode(kPinBatteryLed2, INPUT_ANALOG);
+  pinMode(kPinBatteryLed3, INPUT_ANALOG);
+  pinMode(kPinWhiteLed, INPUT_ANALOG);
+  pinMode(kPinChargeHighCurrentEnable, INPUT_ANALOG);
+
+  Serial1.end();
+
   // When in stop mode, the SysTick interrupt doesn't fire to update Arduino's
   // `millis()` value. So, keep track of the elapsed time using the RTC, and
   // update the value manually on wakeup.
   STM32RTC& rtc = STM32RTC::getInstance();
   rtc_seconds_at_sleep = rtc.getEpoch(&rtc_subseconds_at_sleep);
-  pinMode(kPinScl, INPUT);
-  pinMode(kPinSda, INPUT);
 
-  // Puts the processor into STM32 Stop mode
-  // TODO: reduce power consumption. Currently deepSleep() is ~376uA, and
-  // shutdown() is ~48uA.
-  // TODO: Make sure that the light sensor is actually in standby.
+  // Puts the processor into STM32 Stop mode. Power consumption is ~17uA (as of
+  // 2024-10-26, with hardware v1.1)
   impl_.deepSleep(ms);
-  // impl_.shutdown();
 
   uint32_t seconds;
   uint32_t subseconds;
@@ -51,5 +59,18 @@ void Stm32PowerController::Sleep(uint32_t ms) {
   // underlying millis() directly. This is not great, but it works.
   uwTick += (seconds - rtc_seconds_at_sleep) * 1000 +
             (subseconds - rtc_subseconds_at_sleep);
-  // TODO: trigger I2C re-init?
+
+  pinMode(kPinBatteryLed1, OUTPUT);
+  pinMode(kPinBatteryLed2, OUTPUT);
+  pinMode(kPinBatteryLed3, OUTPUT);
+  pinMode(kPinWhiteLed, OUTPUT);
+  pinMode(kPinChargeHighCurrentEnable, OUTPUT);
+
+  Wire.setSCL(kPinScl);
+  Wire.setSDA(kPinSda);
+  Wire.begin();
+  Wire.setClock(400 * 1000);
+
+  Serial1.begin(115200);
+  Serial1.println("Wakeup");
 }
