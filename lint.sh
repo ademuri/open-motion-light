138edf0ed74829ace84d8c9267dded62771969eb
Adam DeMuri <adam.demuri@gmail.com>
2025-04-02 21:33:42 -0600

    Add lint script

---
+++ lint.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if [ $# -ne 1 ]; then
+    echo "Expected 1 command"
+    echo "Usage: $0 check|format"
+    exit 1
+fi
+
+FILES=$(find \
+lib \
+src \
+-not -path build \
+-iname *.h \
+-o -iname *.cc \
+)
+
+if [ $1 = "check" ]; then
+    clang-format --dry-run --Werror ${FILES?}
+elif [ $1 = "format" ]; then
+    clang-format --style=google -i ${FILES?}
+elif [ $1 = "tidy" ]; then
+    clang-tidy ${FILES?}
+else
+    echo "Unrecognized command: $1"
+    echo "Usage: $0 check|format"
+    exit 1
+fi
